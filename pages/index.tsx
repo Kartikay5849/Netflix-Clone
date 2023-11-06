454f3450cc61ad061f23c104fe636ae8262ff073
Kartikay5849 <kartikaysambher@gmail.com>
2023-11-06 21:33:53 +0530

    profile and routing updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,31 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { NextPageContext } from "next/types"
+import {getSession,signOut} from "next-auth/react"
+import useCurrentUser from "@/hooks/useCurrentUser";
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
 
-const inter = Inter({ subsets: ['latin'] })
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
+}
+
 
 export default function Home() {
+  const {data:user}=useCurrentUser();
   return (
    <div>
-    homee
+   <h1 className="text-4xl text-blue-400"> Netflix </h1>
+   <p className="text-2xl p-4 text-gray-200">Logged in as {user?.name}</p>
+   <button onClick={()=>signOut()} className="w-full text-3xl p-3 h-auto bg-red-300 text-white text-cecnter" >Logout</button>
    </div>
   )
 }
