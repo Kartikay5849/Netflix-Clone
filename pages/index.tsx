d66014ac1fcada3eb8ea2dbbf83d4ac65e76f4aa
Kartikay5849 <kartikaysambher@gmail.com>
2023-11-06 22:23:23 +0530

    Navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { NextPageContext } from "next/types"
 import {getSession,signOut} from "next-auth/react"
 import useCurrentUser from "@/hooks/useCurrentUser";
+import Navbar from "@/components/Navbar";
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
 
@@ -23,9 +24,7 @@ export default function Home() {
   const {data:user}=useCurrentUser();
   return (
    <div>
-   <h1 className="text-4xl text-blue-400"> Netflix </h1>
-   <p className="text-2xl p-4 text-gray-200">Logged in as {user?.name}</p>
-   <button onClick={()=>signOut()} className="w-full text-3xl p-3 h-auto bg-red-300 text-white text-cecnter" >Logout</button>
+        <Navbar/>
    </div>
   )
 }
